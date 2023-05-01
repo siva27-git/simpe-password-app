2d76a06f04df36287bb1a1ded19d8d8e3925e96b
Siva <110472674+siva27-git@users.noreply.github.com>
2023-05-01 08:47:49 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Body from "./components/Body";
+
+import './App.css';
+
+const App = () => {
+  
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
+
+};
+
+export default App;
